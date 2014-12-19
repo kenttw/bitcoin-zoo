a215b86fd164f496fb60c559b91785bc858cee09
kentshih <kentshih@etusolution.com>
2014-12-19 15:53:36 +0800

    try to integration django_bitcoin task

--- core/celeryconfig.py
+++ core/celeryconfig.py
@@ -4,11 +4,16 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 
 #celery setting
 from datetime import timedelta
-CELERY_IMPORTS = ('bitcoin_task', )
+CELERY_IMPORTS = ('bitcoin_task' , 'django_bitcoion' , )
 CELERYBEAT_SCHEDULE = {
-    'add-every-1-seconds': {
-        'task': 'bitcoin_task.tasks.query_transactions',
-        'schedule': timedelta(seconds=1),
-        'args': (16, 16)
+#     'add-every-1-seconds': {
+#         'task': 'bitcoin_task.tasks.query_transactions',
+#         'schedule': timedelta(seconds=10),
+#         'args': (16, 16)
+#     },
+    'syncaddress-every-1-seconds': {
+        'task': 'django_bitcoin.tasks.query_transactions',
+        'schedule': timedelta(seconds=10),
+#         'args': (16, 16)
     },
 }
